0f10359f5580cecedfd97291055d4015d1e1adbe
Anartz Mugika Ledo <amugika@atsistemas.com>
2022-10-31 15:25:18 +0100

    feat: create simple hello file to first tree create

--- hello/src/hello/index.ts
+++ hello/src/hello/index.ts
@@ -1,10 +1,21 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
+/**
+ * Función factory donde se especifican las reglas de la 
+ * colección del schematics con el que vamos a trabajar.
+ * Referencia desde collections.json => "factory"
+ * @param _options 
+ * @returns 
+ */
 export function hello(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    // Función para alterar en nuestro proyecto
+    // para crear el fichero "hello.js" con el log especificado
+    // posteriormente
+    tree.create(
+      'hello.js',
+      "console.log('Hola, estamos trabajando con nuestro primer schematic')"
+    )
     return tree;
   };
 }
